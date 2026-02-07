fe52df820f596f4ba21f5b25b3511a05458631b9
Deep7828 <heydeeppatel@gmail.com>
2026-02-07 02:17:18 -0800

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async ()=> {
-  await mongoose.connect("mongodb://localhost:27017/mydata")
+  await mongoose.connect("process.env.MONGO_URI")
   .then(() => {console.log("DB connected")})
 }
 
